981c54a97947dac85df7656ee746309e597f8611
lucas-kronemberger-uff <lkronemberger@id.uff>
2026-03-30 10:37:29 -0300

    Verificando estrutura de dados e análises descritivas - vars. qualitativas

--- analise-simples.R
+++ analise-simples.R
@@ -3,8 +3,16 @@
 #Leitura do banco de dados
 dados = read_csv2(file= "dados - respostas formulário.csv")
 
-dados %>% 
-  View()
+# Verificando estrutura dos dados
+str(dados)
 
-
-
+# Análises descritivas - variáveis qualitativas
+prop.table(table(ingresso))
+prop.table(table(sexo))
+prop.table(table(grupo_sanguineo))
+prop.table(table(ingresso))
+prop.table(table(ingere_pao))
+prop.table(table(ingere_massa))
+prop.table(table(ingere_refri))
+prop.table(table(ingere_alcool))
+prop.table(table(fruta_gosta))
