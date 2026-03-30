e4d756f71f8ab735091958122e34dadf5a03e58d
lucas-kronemberger-uff <lkronemberger@id.uff>
2026-03-30 10:14:50 -0300

    Script inicial com leitura de banco de dados

---
+++ analise-simples.R
@@ -0,0 +1,10 @@
+#Script referente a aula prática de 30/03 de Git e GitHub, vinculado ao projeto Praticando
+
+#Leitura do banco de dados
+dados = read_csv2(file= "dados - respostas formulário.csv")
+
+dados %>% 
+  View()
+
+
+
