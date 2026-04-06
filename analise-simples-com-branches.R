28b7925a8b578aebdeb1f14261a97d2aa0f7efc4
lucas-kronemberger-uff <lkronemberger@id.uff.br>
2026-04-06 10:19:08 -0300

    Adicionados análise de frequências e gráficos

--- analise-simples-com-branches.R
+++ analise-simples-com-branches.R
@@ -19,9 +19,11 @@ dados$faixa_etaria = factor(dados$faixa_etaria)
 
 # 2. Distribuição de frequências e gráficos
 
-# (PREENCHER NA BRANCH analise-graficos,)
-
-# use comandos para distribuição de frequências de sexo, gráfico de colunas para faixa_etaria por sexo, histograma de altura, boxplot de IMC por sexo
+table(dados$sexo)
+barplot(table(dados$faixa_etaria, dados$sexo), beside = TRUE, legend
+        = TRUE)
+hist(dados$altura)
+boxplot(dados$imc ~ dados$sexo)
 
 # 3. Medidas descritivas
 
